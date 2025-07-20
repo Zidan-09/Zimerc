6dc504885c44a9e29c1876aa97bfb5c64a37a59c
Zidan-09 <samuel.profissional18@gmail.com>
2025-07-20 16:58:45 -0300

    Cadastro com dob

--- backend/src/services/user/registerUser.ts
+++ backend/src/services/user/registerUser.ts
@@ -4,13 +4,13 @@ import { ResultSetHeader } from 'mysql2';
 import { Security } from "../../utils/server/security";
 
 export async function RegisterUser(data: User) {
-    const sql: string = 'INSERT INTO user (name, cpf, email, password, user_type, company_id) VALUES (?, ?, ?, ?, ?, ?)';
+    const sql: string = 'INSERT INTO user (name, cpf, dob, email, password, user_type, company_id) VALUES (?, ?, ?, ?, ?, ?, ?)';
 
     try {
         const password = await Security.hash(data.password);
         const companyId = data.company_id ?? null;
 
-        const [result] = await db.execute<ResultSetHeader>(sql, [data.name, data.cpf, data.email, password, data.user_type, companyId]);
+        const [result] = await db.execute<ResultSetHeader>(sql, [data.name, data.cpf, data.dob, data.email, password, data.user_type, companyId]);
         const id: number = result.insertId;
 
         if (id) {
