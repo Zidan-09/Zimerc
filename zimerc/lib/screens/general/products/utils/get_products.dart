aca5ccc62c1356df1579a2646fd5531dc5012420
Zidan-09 <samuel.profissional18@gmail.com>
2025-08-26 20:09:08 -0300

    Refazendo com mais robustez

--- zimerc/lib/screens/general/products/utils/get_products.dart
+++
@@ -1,5 +0,0 @@
-import '../../../../utils/db.dart';
-
-void getItens() async {
-  await LocalDatabase().database;
-}
--- zimerc/lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFF388E3C);
-  static const Color secundary = Colors.white;
-}
